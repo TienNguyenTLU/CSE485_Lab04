c18ebbd1197285abdf5c60b27caf1ab4ba34e5ef
kaizu <vqkhaibill2004@gmail.com>
2024-12-24 10:13:35 +0700

    update validate ReaderController bai1

--- bai1/app/Http/Controllers/ReaderController.php
+++ bai1/app/Http/Controllers/ReaderController.php
@@ -20,6 +20,21 @@ class ReaderController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => ['required', 'regex:/^[\p{L}\p{N}\s\.,-]+$/u'],
+            'birthday' => ['required', 'date'],
+            'address' => ['required'],
+            'phone' => ['required', 'regex:/^[0-9]+$/'],
+        ],[
+            'name.required' => 'Tên không được để trống.',
+            'name.regex' => 'Tên chỉ được chứa chữ cái, khoảng trắng và không chứa ký tự đặc biệt.',
+            'birthday.required' => 'Ngày sinh không được để trống.',
+            'birthday.date' => 'Ngày sinh không đúng định dạng.',
+            'address.required' => 'Địa chỉ không được để trống.',
+            'phone.required' => 'Số điện thoại không được để trống.',
+            'phone.regex' => 'Số điện thoại chỉ được chứa các chữ số.',
+        ]);
+
         $reader = $request->all();
         Reader::create($reader);
         return redirect()->route('readers.index')->with('success', 'Thêm độc giả thành công!');
@@ -39,6 +54,20 @@ class ReaderController extends Controller
 
     public function update(Request $request, string $id)
     {
+        $request->validate([
+            'name' => ['required', 'regex:/^[\p{L}\p{N}\s\.,-]+$/u'],
+            'birthday' => ['required', 'date'],
+            'address' => ['required'],
+            'phone' => ['required', 'regex:/^[0-9]+$/'],
+        ],[
+            'name.required' => 'Tên không được để trống.',
+            'name.regex' => 'Tên chỉ được chứa chữ cái, khoảng trắng và không chứa ký tự đặc biệt.',
+            'birthday.required' => 'Ngày sinh không được để trống.',
+            'birthday.date' => 'Ngày sinh không đúng định dạng.',
+            'address.required' => 'Địa chỉ không được để trống.',
+            'phone.required' => 'Số điện thoại không được để trống.',
+            'phone.regex' => 'Số điện thoại chỉ được chứa các chữ số.',
+        ]);
         $reader = Reader::find($id);
         $reader->update($request->all());
         return redirect()->route('readers.index')->with('success', 'Cập nhật độc giả thành công!');
